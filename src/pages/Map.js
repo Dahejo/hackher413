1dd43db05dcf58a35fc35450fe46b44a74cb72ec
hejin-jeong <pearlofthesea25@gmail.com>
2022-02-05 13:50:18 -0500

    add pages

---
+++ src/pages/Map.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Map () {
+    return <h1>This is Map</h1>;
+}
+
+export default Map;
---
+++ src/pages/Moma.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Moma() {
+    return <h1>MoMA</h1>
+}
+
+export default Moma;
