d456c73484a758f84bd0f3bb0a1278a0ab40497a
breakndream <kang26d@mtholyoke.edu>
2022-02-06 01:54:50 -0500

    I worked on real-home and nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Navbar.css"
 
 export default function Navbar() {
     return (
-        <div>
+        <div className="realyellow">
             <div className="flex justify-space-between">
-                <h1>Must-seeum</h1>
-                <i class="far fa-heart"></i>
-                <button>Log in</button>
-                <button>Sign up</button>
+                <h1 className="name">Must-seum</h1>
+                <div>
+                    <img className="mona" src="resources/images/mona-lisa.png" alt="mona-lisa" href=""></img>
+                    <button className="login">Log in</button>
+                    <button className="signup">Sign up</button>
+                </div>
             </div>
         </div>
     )
