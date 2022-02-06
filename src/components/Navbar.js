b1caba77f91677a36b3c93abd519c6784f59b06f
hejin-jeong <pearlofthesea25@gmail.com>
2022-02-06 01:57:26 -0500

    design home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,10 @@ export default function Navbar() {
         <div>
             <div className="flex justify-space-between">
                 <h1>Must-seeum</h1>
-                <i class="far fa-heart"></i>
-                <button>Log in</button>
-                <button>Sign up</button>
+                <div>
+                    <button>Log in</button>
+                    <button>Sign up</button>
+                </div>
             </div>
         </div>
     )
