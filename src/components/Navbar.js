4bdfb9e98d9f36212e56fa4254dc8c59522ad4ba
hejin-jeong <pearlofthesea25@gmail.com>
2022-02-05 23:20:05 -0500

    deploy firebase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import React from "react";
 export default function Navbar() {
     return (
         <div>
-            <h1>Must-seeum</h1>
-            <i class="far fa-heart"></i>
-            <button>Log in</button>
-            <button>Sign up</button>
+            <div className="flex justify-space-between">
+                <h1>Must-seeum</h1>
+                <i class="far fa-heart"></i>
+                <button>Log in</button>
+                <button>Sign up</button>
+            </div>
         </div>
     )
 };
